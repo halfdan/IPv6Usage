ef989b09968a862345f7af82e88bbdca6cc703fd
Fabian Becker <halfdan@xnorfz.de>
2012-10-22 22:03:35 +0000

    Improve en.php

--- lang/en.php
+++ lang/en.php
@@ -1,20 +1,18 @@
-<?php 
+<?php
 /**
  * Piwik - Open source web analytics
- * 
- * @link http://piwik.org
+ *
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
- * @version $Id: en.php 6848 2012-08-20 23:18:38Z capedfuzz $
- * 
+ *
  * @category Piwik_Plugins
- * @package Piwik_ExamplePlugin
+ * @package Piwik_IPv6Usage
  */
 
 $translations = array(
-	'IPv6Usage_PluginDescription' => 'IPv6 Usage: This plugin shows how to create a very simple plugin, that exports two widgets in the Dashboard.',
-	'IPv6Usage_ProtocolUsageEvolution' => 'IP Protocol Usage Over Time',
+	'IPv6Usage_PluginDescription' => 'IPv6 Usage: This plugin shows the number of visits via IPv4 and IPv6',
+	'IPv6Usage_ProtocolUsageEvolution' => 'IP Protocol Evolution',
 	'IPv6Usage_WidgetDescription' => '',
-	'IPv6Usage_WidgetProtocolDescription' => 'IP Protocol Usage',
+	'IPv6Usage_WidgetProtocolDescription' => 'IP Protocol by Version',
 	'IPv6Usage_IPv4' => 'IPv4',
 	'IPv6Usage_IPv6' => 'IPv6',
 	'IPv6Usage_IPv4_rate' => 'Rate of IPv4 visits',
